23006b62392e43069c62727ec4693724c2233e1c
bagas541 <balics@mail.ru>
2023-10-04 20:26:24 +0300

    add databaseConnection class and realize method for connection to the database

---
+++ lib/src/main/java/_Bank_remaster/db/DatabaseConnection.java
@@ -0,0 +1,35 @@
+package _Bank_remaster.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+	private static final String URL = "jdbc:postgresql://localhost:5432/clever_bank";
+	private static final String USERNAME = "bagas";
+	private static final String PASSWORD = "12345";
+	
+	public Connection getConnection() {
+		try {
+			Class.forName("org.postgresql.Driver");
+			return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException("Error with connecting to database");
+		} 
+	}
+	
+	public static void closeConnection(Connection connection) {
+		if(connection != null) {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
