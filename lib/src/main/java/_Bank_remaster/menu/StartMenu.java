8a0e99e4783e7fef034e748366e80ec12b80ab4b
bagas541 <balics@mail.ru>
2023-10-13 22:15:01 +0300

    finished start menu and transaction menu

---
+++ lib/src/main/java/_Bank_remaster/menu/StartMenu.java
@@ -0,0 +1,37 @@
+package _Bank_remaster.menu;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import _Bank_remaster.db.DatabaseConnection;
+
+public class StartMenu extends Menu {
+
+	private final String MENU = """
+			\n----------------------------
+			1: CRUD-операции
+			2: Пользователь
+			----------------------------\n""" ;
+	
+
+	@Override
+	public void start() {
+		try(Connection connection = DatabaseConnection.getConnection()) {
+			while(true) {
+				printMenu(MENU);
+				int chosen = scanner.nextInt();
+				if(chosen == 1) {
+					CRUDMenu crudMenu = new CRUDMainMenu(connection);
+					crudMenu.pushMenu(crudMenu);
+					crudMenu.start();
+				} else if(chosen == 2) {
+					Menu mainMenu = new MainMenu(connection);
+					mainMenu.start();
+				}
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	}
+
+}
